ee9308216d39789f0623349e96d168630b2c7590
Bassant Ashraf <bassant.ashraf@andela.com>
2020-05-01 05:53:57 +0200

    Initial Commit

---
+++ FlickerApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlickerApp
+//
+//  Created by Bassant Ashraf on 5/1/20.
+//  Copyright © 2020 Bassant Ashraf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
